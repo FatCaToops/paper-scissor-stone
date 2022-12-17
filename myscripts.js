56f24c22d7bf400eb7684d4bfb0e0a308b73ac9a
Hanis <workingspecific@gmail.com>
2022-12-16 23:55:07 -0800

    update UI: move the results from console to the page and add the playing details

--- myscripts.js
+++ myscripts.js
@@ -6,19 +6,25 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     //show the result of each round
+    let cWin = "Computer wins this round";
+    let pWin = "You win this round";
+    let tie = "Result is tie";
     switch(playerSelection){
         case 'stone':
             switch(computerSelection){
                 case 'stone':
                     console.log('Tie');
+                    return tie;
                     break;
                 case 'scissor':
                     console.log('You win');
                     playerPoint++;
+                    return pWin;
                     break;
                 default: //'paper'
                     console.log('You lose');
                     computerPoint++;
+                    return cWin;
             }
             break;
         case 'paper':
@@ -26,13 +32,16 @@ function playRound(playerSelection, computerSelection) {
                 case 'stone':
                     console.log('You win');
                     playerPoint++;
+                    return pWin;
                     break;
                 case 'scissor':
                     console.log('You lose');
                     computerPoint++;
+                    return cWin;
                     break;
                 default: //'paper'
                     console.log('Tie');
+                    return tie;
             }
             break;
         case 'scissor':
@@ -40,13 +49,16 @@ function playRound(playerSelection, computerSelection) {
                 case 'stone':
                     console.log('You lose');
                     computerPoint++;
+                    return cWin;
                     break;
                 case 'scissor':
                     console.log('Tie');
+                    return tie;
                     break;
                 default: //'paper'
-                    console.log('You won');
+                    console.log('You win');
                     playerPoint++;
+                    return pWin;
             }
             break;
         default:
@@ -54,77 +66,102 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function updatePoint(parent, node, pPoint, cPoint) {
+    parent.removeChild(node);
+    let displayPoint = document.createElement('p');
+    let detail = document.querySelector('.details');
+    displayPoint.innerText = pPoint + ":" + cPoint;
+    displayPoint.className = "vsPoint";
+    parent.insertBefore(displayPoint, detail);
+}
+
+function updateDetail(pSelection, cSelection, res) {
+    let list = document.createElement('li');
+    let detail = document.querySelector('.details');
+    
+    list.innerText = "Your choice is " + pSelection + ". "
+                    + "Computer's choice is " + cSelection + ". "
+                    + res;
+    detail.appendChild(list);
+}
+
 const gameNum = 
-prompt("How many games do you/computer need to win to end the game?")
+prompt("How many rounds do you/computer need to win?")
 
 let playerPoint = 0;
 let computerPoint = 0;
 
 let playerSelection = '';
 
-const paper = document.getElementById('paper');
-const scissor = document.querySelector('#scissor');
-const stone = document.querySelector('#stone');
+// get button
+let paper = document.getElementById('paper');
+let scissor = document.querySelector('#scissor');
+let stone = document.querySelector('#stone');
 
-const urPoint = document.createElement('p')
-// infinite loop when using while loop
-/*while ((playerPoint !== gameNum) || (computerPoint !== gameNum)){
+let points = document.querySelector('.points');
+let vsPoint = document.querySelector('.vsPoint');
+let btn = document.querySelector('.button');
+
+paper.addEventListener("click", (e) => {
+    e.preventDefault();
+    const computerSelection = getComputerChoice().toLowerCase();
+    console.log("Computer's choice is " + computerSelection);
+    
+    playerSelection = 'paper';
+    console.log("Your choice is " + playerSelection);
+    let result = playRound(playerSelection, computerSelection);
+    console.log(playerPoint);
+    console.log(computerPoint);
+
+    vsPoint = document.querySelector('.vsPoint');
+    updatePoint(points, vsPoint, playerPoint, computerPoint);
+    updateDetail(playerSelection, computerSelection, result);
     if (playerPoint == gameNum) {
-        break;
+        console.log("You win this game !!");
     }
     if (computerPoint == gameNum) {
-        break;
-    }*/
-    
-    paper.addEventListener("click", (e) => {
-        e.preventDefault();
-        const computerSelection = getComputerChoice().toLowerCase();
-        console.log("Computer's choice is " + computerSelection);
-        
-        playerSelection = 'paper';
-        console.log("Your choice is " + playerSelection);
-        playRound(playerSelection, computerSelection);
-        console.log(playerPoint);
-        console.log(computerPoint);
-        if (playerPoint == gameNum) {
-            console.log("You win this game !!");
-        }
-        if (computerPoint == gameNum) {
-            console.log("Computer wins this game !!");
-        }
-    });
-    scissor.addEventListener("click", (e) => {
-        e.preventDefault();
-        const computerSelection = getComputerChoice().toLowerCase();
-        console.log("Computer's choice is " + computerSelection);
+        console.log("Computer wins this game !!");
+    }
+});
+scissor.addEventListener("click", (e) => {
+    e.preventDefault();
+    const computerSelection = getComputerChoice().toLowerCase();
+    console.log("Computer's choice is " + computerSelection + " .");
 
-        playerSelection = 'scissor';
-        console.log("Your choice is " + playerSelection);
-        playRound(playerSelection, computerSelection);
-        console.log(playerPoint);
-        console.log(computerPoint);
-        if (playerPoint == gameNum) {
-            console.log("You win this game !!");
-        }
-        if (computerPoint == gameNum) {
-            console.log("Computer wins this game !!");
-        }
-    });
-    stone.addEventListener("click", (e) => {
-        e.preventDefault();
-        const computerSelection = getComputerChoice().toLowerCase();
-        console.log("Computer's choice is " + computerSelection);
+    playerSelection = 'scissor';
+    console.log("Your choice is " + playerSelection);
+    let result = playRound(playerSelection, computerSelection);
+    console.log(playerPoint);
+    console.log(computerPoint);
 
-        playerSelection = 'stone';
-        console.log("Your choice is " + playerSelection);
-        playRound(playerSelection, computerSelection);
-        console.log(playerPoint);
-        console.log(computerPoint);
-        if (playerPoint == gameNum) {
-            console.log("You win this game !!");
-        }
-        if (computerPoint == gameNum) {
-            console.log("Computer wins this game !!");
-        }
-    });
-//}
+    vsPoint = document.querySelector('.vsPoint');
+    updatePoint(points, vsPoint, playerPoint, computerPoint);
+    updateDetail(playerSelection, computerSelection, result);
+    if (playerPoint == gameNum) {
+        console.log("You win this game !!");
+    }
+    if (computerPoint == gameNum) {
+        console.log("Computer wins this game !!");
+    }
+});
+stone.addEventListener("click", (e) => {
+    e.preventDefault();
+    const computerSelection = getComputerChoice().toLowerCase();
+    console.log("Computer's choice is " + computerSelection + " .");
+
+    playerSelection = 'stone';
+    console.log("Your choice is " + playerSelection);
+    let result = playRound(playerSelection, computerSelection);
+    console.log(playerPoint);
+    console.log(computerPoint);
+
+    vsPoint = document.querySelector('.vsPoint');
+    updatePoint(points, vsPoint, playerPoint, computerPoint);
+    updateDetail(playerSelection, computerSelection, result);
+    if (playerPoint == gameNum) {
+        console.log("You win this game !!");
+    }
+    if (computerPoint == gameNum) {
+        console.log("Computer wins this game !!");
+    }
+});
